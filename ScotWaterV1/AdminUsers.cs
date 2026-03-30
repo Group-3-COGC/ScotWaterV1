c9054b9b825ba4ec6702225b65875004ce4f4365
30177459 <30177459@C0560CB0022007>
2026-03-30 13:05:46 +0100

    Admi user class

---
+++ ScotWaterV1/AdminUsers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScotWaterV1
+{
+    public class AdminUsers
+    {
+            public int AdminID { get; set; }
+            public string AdminUsername { get; set; }
+            public string AdminPassword { get; set; }
+    }
+}
