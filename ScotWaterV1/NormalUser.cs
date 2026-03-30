d3a1f9872dd3c2a757f20fc95b5ca20b9e91d15c
Jack <30504157@C0560CB0021909>
2026-03-30 13:46:01 +0100

    user class

---
+++ ScotWaterV1/NormalUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScotWaterV1
+{
+    public class NormalUser
+    {
+        //get; set; attributes for normal users
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+        public string ContactName { get; set; }
+        public int ContactNumber { get; set; }
+        public string ContactEmail { get; set; }
+        public string AccountName { get; set; }
+
+
+    }
+}
