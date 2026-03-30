53154996da1e14ad1459ee949144d6f836dfbbdf
30177459 <30177459@C0560CB0022007>
2026-03-30 13:49:58 +0100

    Admin user class

---
+++ ScotWaterV1/WaterUsage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ScotWaterV1
+{
+    public class WaterUsage
+    {
+        public int FreshwaterUnitsUsed { get; set; }
+
+        // True = Low Reserve, False = Standard
+        public bool IsLowReserve { get; set; }
+
+        public int RecycledUnits { get; set; }
+
+
+
+       
+
+        
+    }
+}
