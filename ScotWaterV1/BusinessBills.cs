5029a18e6843a0d2e8feb228c9ef6b7e1ef9416d
Leon <30536743@C0560CB0021928>
2026-03-30 13:52:37 +0100

    BusinessBills

---
+++ ScotWaterV1/BusinessBills.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScotWaterV1
+{
+    public class BusinessBills
+    {
+        public string BillingPeriod { get; set; }
+        public double TotalCharges { get; set; }
+        public double TotalDiscount { get; set; }
+        public double DiscountRate { get; set; }
+        public double SubTotal { get; set; }
+        public double VAT { get; set; }
+        public double BusinessFinalCost { get; set; }
+
+
+    }
+}
