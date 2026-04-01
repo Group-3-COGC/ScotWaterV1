0f5d31270ac8c4c6da66c16709440bdb84907919
30177459 <30177459@C0560CB0022007>
2026-04-01 13:24:03 +0100

    push

--- ScotWaterV1/MainMenu.Designer.cs
+++ ScotWaterV1/MainMenu.Designer.cs
@@ -155,6 +155,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(984, 611);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.lblWelcome);
