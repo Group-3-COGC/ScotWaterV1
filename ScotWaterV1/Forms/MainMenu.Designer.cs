33cfa7e480350dab7e09c32198830565867adff9
30177459 <30177459@C0560CB0022007>
2026-04-01 14:51:01 +0100

    Business Form

--- ScotWaterV1/Forms/MainMenu.Designer.cs
+++ ScotWaterV1/Forms/MainMenu.Designer.cs
@@ -86,6 +86,7 @@
             this.btnViewBusinesses.TabIndex = 4;
             this.btnViewBusinesses.Text = "View Businesses ";
             this.btnViewBusinesses.UseVisualStyleBackColor = true;
+            this.btnViewBusinesses.Click += new System.EventHandler(this.btnViewBusinesses_Click);
             // 
             // btnModifyWaterPrice
             // 
