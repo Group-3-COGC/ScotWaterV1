015deb7e46bf421625873c7181bca3ede4c59768
30177459 <30177459@C0560CB0022007>
2026-04-02 14:23:46 +0100

    Revert "view business"
    This reverts commit 0749e54977df2c4142fd73edc24fb884ed048e89.

--- ScotWaterV1/Forms/ViewBusinesses.cs
+++ ScotWaterV1/Forms/ViewBusinesses.cs
@@ -16,10 +16,5 @@ namespace ScotWaterV1.Forms
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
