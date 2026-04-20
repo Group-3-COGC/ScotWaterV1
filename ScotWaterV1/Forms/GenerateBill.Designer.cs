9a63e0352423996f2d5e367bc07667658b1c3904
antoinmcarthur <30565188@C0560CB0021672>
2026-04-20 10:53:29 +0100

    commit

--- ScotWaterV1/Forms/GenerateBill.Designer.cs
+++ ScotWaterV1/Forms/GenerateBill.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ScotWaterV1.Forms
 {
-    partial class GenerateBill
+    partial class frmGenerateBill
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGenerateBill));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnG_B_SignOut = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnG_B_MainMenu = new System.Windows.Forms.Button();
+            this.lblSelectBuisnessGB = new System.Windows.Forms.Label();
+            this.cbxBuisnessNamesGB = new System.Windows.Forms.ComboBox();
+            this.lblUnitsUsedGB = new System.Windows.Forms.Label();
+            this.txtUnitsUsedGB = new System.Windows.Forms.TextBox();
+            this.lblRecycledUnitsGB = new System.Windows.Forms.Label();
+            this.txtRecycledGB = new System.Windows.Forms.TextBox();
+            this.dateTimePicker2GB = new System.Windows.Forms.DateTimePicker();
+            this.lblGenerateBillsDate = new System.Windows.Forms.Label();
+            this.lblGeneateBillTotal = new System.Windows.Forms.Label();
+            this.txtGenerateBillTotal = new System.Windows.Forms.TextBox();
+            this.btnGenerateBill = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(802, 104);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, -2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(152, 104);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnG_B_SignOut
+            // 
+            this.btnG_B_SignOut.BackColor = System.Drawing.Color.White;
+            this.btnG_B_SignOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnG_B_SignOut.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnG_B_SignOut.Location = new System.Drawing.Point(675, 26);
+            this.btnG_B_SignOut.Margin = new System.Windows.Forms.Padding(2);
+            this.btnG_B_SignOut.Name = "btnG_B_SignOut";
+            this.btnG_B_SignOut.Size = new System.Drawing.Size(100, 35);
+            this.btnG_B_SignOut.TabIndex = 17;
+            this.btnG_B_SignOut.Text = "Sign Out";
+            this.btnG_B_SignOut.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, 391);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(802, 64);
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnG_B_MainMenu
+            // 
+            this.btnG_B_MainMenu.BackColor = System.Drawing.Color.White;
+            this.btnG_B_MainMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnG_B_MainMenu.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnG_B_MainMenu.Location = new System.Drawing.Point(329, 404);
+            this.btnG_B_MainMenu.Margin = new System.Windows.Forms.Padding(2);
+            this.btnG_B_MainMenu.Name = "btnG_B_MainMenu";
+            this.btnG_B_MainMenu.Size = new System.Drawing.Size(100, 35);
+            this.btnG_B_MainMenu.TabIndex = 19;
+            this.btnG_B_MainMenu.Text = "Main Menu ";
+            this.btnG_B_MainMenu.UseVisualStyleBackColor = false;
+            // 
+            // lblSelectBuisnessGB
+            // 
+            this.lblSelectBuisnessGB.AutoSize = true;
+            this.lblSelectBuisnessGB.Location = new System.Drawing.Point(27, 137);
+            this.lblSelectBuisnessGB.Name = "lblSelectBuisnessGB";
+            this.lblSelectBuisnessGB.Size = new System.Drawing.Size(92, 13);
+            this.lblSelectBuisnessGB.TabIndex = 20;
+            this.lblSelectBuisnessGB.Text = "Select A Buisness";
+            // 
+            // cbxBuisnessNamesGB
+            // 
+            this.cbxBuisnessNamesGB.FormattingEnabled = true;
+            this.cbxBuisnessNamesGB.Location = new System.Drawing.Point(169, 134);
+            this.cbxBuisnessNamesGB.Name = "cbxBuisnessNamesGB";
+            this.cbxBuisnessNamesGB.Size = new System.Drawing.Size(121, 21);
+            this.cbxBuisnessNamesGB.TabIndex = 21;
+            // 
+            // lblUnitsUsedGB
+            // 
+            this.lblUnitsUsedGB.AutoSize = true;
+            this.lblUnitsUsedGB.Location = new System.Drawing.Point(27, 195);
+            this.lblUnitsUsedGB.Name = "lblUnitsUsedGB";
+            this.lblUnitsUsedGB.Size = new System.Drawing.Size(110, 13);
+            this.lblUnitsUsedGB.TabIndex = 22;
+            this.lblUnitsUsedGB.Text = "Amount of Units Used";
+            // 
+            // txtUnitsUsedGB
+            // 
+            this.txtUnitsUsedGB.Location = new System.Drawing.Point(169, 192);
+            this.txtUnitsUsedGB.Name = "txtUnitsUsedGB";
+            this.txtUnitsUsedGB.Size = new System.Drawing.Size(100, 20);
+            this.txtUnitsUsedGB.TabIndex = 23;
+            // 
+            // lblRecycledUnitsGB
+            // 
+            this.lblRecycledUnitsGB.AutoSize = true;
+            this.lblRecycledUnitsGB.Location = new System.Drawing.Point(27, 240);
+            this.lblRecycledUnitsGB.Name = "lblRecycledUnitsGB";
+            this.lblRecycledUnitsGB.Size = new System.Drawing.Size(130, 13);
+            this.lblRecycledUnitsGB.TabIndex = 24;
+            this.lblRecycledUnitsGB.Text = "Amount of Units Recycled";
+            // 
+            // txtRecycledGB
+            // 
+            this.txtRecycledGB.Location = new System.Drawing.Point(169, 237);
+            this.txtRecycledGB.Name = "txtRecycledGB";
+            this.txtRecycledGB.Size = new System.Drawing.Size(100, 20);
+            this.txtRecycledGB.TabIndex = 25;
+            // 
+            // dateTimePicker2GB
+            // 
+            this.dateTimePicker2GB.Location = new System.Drawing.Point(169, 288);
+            this.dateTimePicker2GB.Name = "dateTimePicker2GB";
+            this.dateTimePicker2GB.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2GB.TabIndex = 26;
+            // 
+            // lblGenerateBillsDate
+            // 
+            this.lblGenerateBillsDate.AutoSize = true;
+            this.lblGenerateBillsDate.Location = new System.Drawing.Point(28, 295);
+            this.lblGenerateBillsDate.Name = "lblGenerateBillsDate";
+            this.lblGenerateBillsDate.Size = new System.Drawing.Size(91, 13);
+            this.lblGenerateBillsDate.TabIndex = 27;
+            this.lblGenerateBillsDate.Text = "Select Date of Bill";
+            // 
+            // lblGeneateBillTotal
+            // 
+            this.lblGeneateBillTotal.AutoSize = true;
+            this.lblGeneateBillTotal.Location = new System.Drawing.Point(584, 288);
+            this.lblGeneateBillTotal.Name = "lblGeneateBillTotal";
+            this.lblGeneateBillTotal.Size = new System.Drawing.Size(54, 13);
+            this.lblGeneateBillTotal.TabIndex = 28;
+            this.lblGeneateBillTotal.Text = "Total Due";
+            // 
+            // txtGenerateBillTotal
+            // 
+            this.txtGenerateBillTotal.Location = new System.Drawing.Point(560, 320);
+            this.txtGenerateBillTotal.Name = "txtGenerateBillTotal";
+            this.txtGenerateBillTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtGenerateBillTotal.TabIndex = 29;
+            // 
+            // btnGenerateBill
+            // 
+            this.btnGenerateBill.Location = new System.Drawing.Point(512, 134);
+            this.btnGenerateBill.Name = "btnGenerateBill";
+            this.btnGenerateBill.Size = new System.Drawing.Size(179, 98);
+            this.btnGenerateBill.TabIndex = 30;
+            this.btnGenerateBill.Text = "Generate Bill";
+            this.btnGenerateBill.UseVisualStyleBackColor = true;
+            // 
+            // frmGenerateBill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGenerateBill);
+            this.Controls.Add(this.txtGenerateBillTotal);
+            this.Controls.Add(this.lblGeneateBillTotal);
+            this.Controls.Add(this.lblGenerateBillsDate);
+            this.Controls.Add(this.dateTimePicker2GB);
+            this.Controls.Add(this.txtRecycledGB);
+            this.Controls.Add(this.lblRecycledUnitsGB);
+            this.Controls.Add(this.txtUnitsUsedGB);
+            this.Controls.Add(this.lblUnitsUsedGB);
+            this.Controls.Add(this.cbxBuisnessNamesGB);
+            this.Controls.Add(this.lblSelectBuisnessGB);
+            this.Controls.Add(this.btnG_B_MainMenu);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnG_B_SignOut);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmGenerateBill";
             this.Text = "GenerateBill";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnG_B_SignOut;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnG_B_MainMenu;
+        private System.Windows.Forms.Label lblSelectBuisnessGB;
+        private System.Windows.Forms.ComboBox cbxBuisnessNamesGB;
+        private System.Windows.Forms.Label lblUnitsUsedGB;
+        private System.Windows.Forms.TextBox txtUnitsUsedGB;
+        private System.Windows.Forms.Label lblRecycledUnitsGB;
+        private System.Windows.Forms.TextBox txtRecycledGB;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2GB;
+        private System.Windows.Forms.Label lblGenerateBillsDate;
+        private System.Windows.Forms.Label lblGeneateBillTotal;
+        private System.Windows.Forms.TextBox txtGenerateBillTotal;
+        private System.Windows.Forms.Button btnGenerateBill;
     }
 }
