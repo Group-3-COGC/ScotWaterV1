bf3839677ff18959d574ed1e31673eeb9cc57ab0
Dean <30177459@C0560CB0021935>
2026-04-20 14:32:32 +0100

    Dean

--- ScotWaterV1/Repositories/AdminRepository.cs
+++ ScotWaterV1/Repositories/AdminRepository.cs
@@ -6,6 +6,14 @@ namespace ScotWaterV1.Repositories
 {
     public class AdminRepository
     {
-      
+        public AdminUsers Login(string username, string password)
+        {
+            using (var context = new BusinessDataContext())
+            {
+                return context.AdminUsers
+                    .FirstOrDefault(a => a.AdminUsername == username
+                                      && a.AdminPassword == password);
+            }
+        }
     }
 }
--- ScotWaterV1/Repositories/StaffUserRepository.cs
+++ ScotWaterV1/Repositories/StaffUserRepository.cs
@@ -6,6 +6,14 @@ namespace ScotWaterV1.Repositories
 {
     public class StaffUserRepository
     {
-       
+        public StaffUser Login(string username, string password)
+        {
+            using (var context = new BusinessDataContext())
+            {
+                return context.StaffUser
+                    .FirstOrDefault(s => s.staffUsername == username
+                                      && s.staffPassword == password);
+            }
+        }
     }
 }
