e246ccdabed0c69555d4a122cc8cdd96d27c0765
Dean <30177459@C0560CB0021935>
2026-04-23 10:30:21 +0100

    Adding a display bill page

---
+++ ScotWaterV1/Forms/frmDisplayBill.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ScotWaterV1.Forms
+{
+    partial class DisplayBill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DisplayBill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(984, 611);
+            this.Name = "DisplayBill";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
