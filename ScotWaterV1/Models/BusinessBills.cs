5829af1b2a583a499e4d7f9d688384b2015400c2
Dean <30177459@C0560CB0021935>
2026-04-27 11:24:44 +0100

    fix

--- ScotWaterV1/Models/BusinessBills.cs
+++ ScotWaterV1/Models/BusinessBills.cs
@@ -1,34 +1,35 @@
-﻿using System;
+﻿using ScotWaterV1.Models;
+using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace ScotWaterV1.Models
+public class BusinessBills
 {
-    public class BusinessBills
-    {
-        [Key]
-        public int BusinessBillID { get; set; }
+    [Key]
+    public int BusinessBillID { get; set; }
 
-        [Required]
-        public DateTime BillDate { get; set; }
+    [Required]
+    public DateTime BillDate { get; set; }
 
-        [Required]
-        public decimal TotalCharges { get; set; }
+    [Required]
+    public decimal TotalCharges { get; set; }
 
-        [Required]
-        public decimal TotalDiscount { get; set; }
+    [Required]
+    public decimal TotalDiscount { get; set; }
 
-        [Required]
-        public decimal SubTotal { get; set; }
+    [Required]
+    public decimal SubTotal { get; set; }
 
-        [Required]
-        public decimal VAT { get; set; }
+    [Required]
+    public decimal VAT { get; set; }
 
-        [Required]
-        public decimal BusinessFinalCost { get; set; }
+    [Required]
+    public decimal BusinessFinalCost { get; set; }
 
-        [Required]
-        public int BusinessID { get; set; }
+    [Required]
+    public decimal DiscountRate { get; set; }   // ← ADD THIS BACK
 
-        public BusinessUser BusinessUser { get; set; }
-    }
+    [Required]
+    public int BusinessID { get; set; }
+
+    public BusinessUser BusinessUser { get; set; }
 }
