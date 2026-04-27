87b8abcaffa80af7b237b6eb815fdb87379dc736
Dean <30177459@C0560CB0021935>
2026-04-27 13:41:10 +0100

    fix

--- ScotWaterV1/Forms/MainMenu.Designer.cs
+++ ScotWaterV1/Forms/MainMenu.Designer.cs
@@ -259,7 +259,6 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "frmMainMenu";
-            this.Text = "Main Menu";
             this.PanelMenu.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
