e3355589c276194da083079faa88b0c69d148b31
Dean <30177459@C0548CB0021337>
2026-04-29 13:44:57 +0100

    fix

--- ScotWaterV1/Forms/Login.Designer.cs
+++ ScotWaterV1/Forms/Login.Designer.cs
@@ -164,6 +164,7 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
