507da73a636ebdfeef219dd73a83841caf1ded5d
Mohammad <30553902@C0560CB0021982>
2026-04-29 15:22:12 +0100

    hhh

--- ScotWaterV1/Forms/GenerateBill.Designer.cs
+++ ScotWaterV1/Forms/GenerateBill.Designer.cs
@@ -148,7 +148,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 611);
+            this.ClientSize = new System.Drawing.Size(998, 611);
             this.Controls.Add(this.btnGenerateBill);
             this.Controls.Add(this.lblGenerateBillsDate);
             this.Controls.Add(this.dtpBillDate);
