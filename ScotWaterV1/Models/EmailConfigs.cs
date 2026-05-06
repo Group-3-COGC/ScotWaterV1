b876ce1bed061fb242519a477eaddef482ff59bb
antoinmcarthur <30565188@C0560CB0021672>
2026-05-06 13:38:06 +0100

    email

---
+++ ScotWaterV1/Models/EmailConfigs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScotWaterV1.Models
+{
+    public class EmailConfigs
+    {
+        [Key]
+        public int EmailSettingsID { get; set; }
+        [Required]
+        public string SenderEmail { get; set; }
+        [Required]
+        public string SenderPassword { get; set; }
+        [Required]
+        public string SmtpHost { get; set; }
+        [Required]
+        public int SmtpPort { get; set; }
+        
+        public bool EnableSsl { get; set; }
+
+    }
+}
