138bdb64c87cd07d262cbc64cbd3dbe01e408a12
Mohammad <30553902@C0560CB0021982>
2026-05-11 11:47:33 +0100

    fixes

--- ScotWaterV1/Forms/ChangeWaterLevels.Designer.cs
+++ ScotWaterV1/Forms/ChangeWaterLevels.Designer.cs
@@ -27,43 +27,28 @@
             this.lblValue = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pnlHeader = new System.Windows.Forms.Panel();
-            this.pnlSelect = new System.Windows.Forms.Panel();
             this.lblTitle = new System.Windows.Forms.Label();
-
+            this.pnlSelect = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trkReserveLevel)).BeginInit();
             this.pnlHeader.SuspendLayout();
             this.pnlSelect.SuspendLayout();
             this.SuspendLayout();
-
-            // ================= HEADER =================
-            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(0, 120, 215);
-            this.pnlHeader.Controls.Add(this.lblTitle);
-            this.pnlHeader.Controls.Add(this.pictureBox3);
-            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlHeader.Size = new System.Drawing.Size(1088, 125);
-
+            // 
+            // pictureBox3
+            // 
             this.pictureBox3.BackColor = System.Drawing.Color.White;
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
             this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(182, 125);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold);
-            this.lblTitle.ForeColor = System.Drawing.Color.White;
-            this.lblTitle.Location = new System.Drawing.Point(243, 42);
-            this.lblTitle.Text = "Change Water Levels";
-
-            // ================= PANEL =================
-            this.pnlSelect.BackColor = System.Drawing.Color.White;
-            this.pnlSelect.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlSelect.Location = new System.Drawing.Point(250, 160);
-            this.pnlSelect.Size = new System.Drawing.Size(580, 280);
-            this.pnlSelect.Controls.Add(this.groupBox1);
-
-            // ================= GROUPBOX =================
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // groupBox1
+            // 
             this.groupBox1.Controls.Add(this.btnSave);
             this.groupBox1.Controls.Add(this.lblWaterReserveStatus);
             this.groupBox1.Controls.Add(this.trkReserveLevel);
@@ -73,57 +58,121 @@
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
             this.groupBox1.ForeColor = System.Drawing.SystemColors.Highlight;
             this.groupBox1.Location = new System.Drawing.Point(20, 10);
+            this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(540, 260);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
             this.groupBox1.Text = "WATER LEVELS";
-
-            // ================= SAVE BUTTON (FIXED STYLE) =================
-            this.btnSave.BackColor = System.Drawing.Color.FromArgb(0, 102, 204);
-            this.btnSave.ForeColor = System.Drawing.Color.White;
-            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSave.FlatAppearance.BorderSize = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
             this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSave.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnSave.ForeColor = System.Drawing.Color.White;
             this.btnSave.Location = new System.Drawing.Point(420, 210);
+            this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(100, 35);
+            this.btnSave.TabIndex = 0;
             this.btnSave.Text = "SAVE";
             this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-
-            // ================= OTHER CONTROLS (UNCHANGED) =================
+            // 
+            // lblWaterReserveStatus
+            // 
             this.lblWaterReserveStatus.AutoSize = true;
-            this.trkReserveLevel.Maximum = 100;
+            this.lblWaterReserveStatus.Location = new System.Drawing.Point(44, 217);
+            this.lblWaterReserveStatus.Name = "lblWaterReserveStatus";
+            this.lblWaterReserveStatus.Size = new System.Drawing.Size(0, 20);
+            this.lblWaterReserveStatus.TabIndex = 1;
+            // 
+            // trkReserveLevel
+            // 
             this.trkReserveLevel.Location = new System.Drawing.Point(230, 135);
+            this.trkReserveLevel.Maximum = 100;
+            this.trkReserveLevel.Name = "trkReserveLevel";
             this.trkReserveLevel.Size = new System.Drawing.Size(290, 45);
+            this.trkReserveLevel.TabIndex = 2;
             this.trkReserveLevel.Scroll += new System.EventHandler(this.trkReserveLevel_Scroll);
-
+            // 
+            // progressBarReserveLevel
+            // 
             this.progressBarReserveLevel.Location = new System.Drawing.Point(230, 40);
+            this.progressBarReserveLevel.Name = "progressBarReserveLevel";
             this.progressBarReserveLevel.Size = new System.Drawing.Size(290, 40);
-
+            this.progressBarReserveLevel.TabIndex = 3;
+            // 
+            // lblValue
+            // 
             this.lblValue.AutoSize = true;
             this.lblValue.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
             this.lblValue.Location = new System.Drawing.Point(6, 135);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(204, 25);
+            this.lblValue.TabIndex = 4;
             this.lblValue.Text = "Water Reserve Status:";
-
+            // 
+            // label1
+            // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
             this.label1.Location = new System.Drawing.Point(15, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 25);
+            this.label1.TabIndex = 5;
             this.label1.Text = "Water Reserve Level";
-
-            // ================= FORM =================
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.pnlHeader.Controls.Add(this.lblTitle);
+            this.pnlHeader.Controls.Add(this.pictureBox3);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(1088, 125);
+            this.pnlHeader.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(243, 42);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(284, 37);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Change Water Levels";
+            // 
+            // pnlSelect
+            // 
+            this.pnlSelect.BackColor = System.Drawing.Color.White;
+            this.pnlSelect.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSelect.Controls.Add(this.groupBox1);
+            this.pnlSelect.Location = new System.Drawing.Point(250, 160);
+            this.pnlSelect.Name = "pnlSelect";
+            this.pnlSelect.Size = new System.Drawing.Size(580, 280);
+            this.pnlSelect.TabIndex = 0;
+            // 
+            // frmChangeWaterLevels
+            // 
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1088, 601);
             this.Controls.Add(this.pnlSelect);
             this.Controls.Add(this.pnlHeader);
             this.Name = "frmChangeWaterLevels";
             this.Load += new System.EventHandler(this.frmChangeWaterLevels_Load);
-
-            this.pnlHeader.ResumeLayout(false);
-            this.pnlHeader.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trkReserveLevel)).EndInit();
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
             this.pnlSelect.ResumeLayout(false);
             this.ResumeLayout(false);
+
         }
 
         #endregion
