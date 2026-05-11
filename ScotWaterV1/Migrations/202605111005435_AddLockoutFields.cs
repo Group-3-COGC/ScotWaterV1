ecb0231e086c710e7305b2f1ae9c3351e28bebed
Dean <30177459@C0548CB0021337>
2026-05-11 11:29:49 +0100

    fix

--- ScotWaterV1/Migrations/202605111005435_AddLockoutFields.cs
+++ ScotWaterV1/Migrations/202605111005435_AddLockoutFields.cs
@@ -2,21 +2,23 @@
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class AddLockoutFields : DbMigration
     {
         public override void Up()
         {
             AddColumn("dbo.AdminUsers", "FailedLoginAttempts", c => c.Int(nullable: false));
             AddColumn("dbo.AdminUsers", "IsLocked", c => c.Boolean(nullable: false));
-            AddColumn("dbo.StaffUsers", "FailedLoginAttempts", c => c.Int(nullable: false));
-            AddColumn("dbo.StaffUsers", "IsLocked", c => c.Boolean(nullable: false));
+
+            AddColumn("dbo.StaffUser", "FailedLoginAttempts", c => c.Int(nullable: false));
+            AddColumn("dbo.StaffUser", "IsLocked", c => c.Boolean(nullable: false));
         }
-        
+
         public override void Down()
         {
-            DropColumn("dbo.StaffUsers", "IsLocked");
-            DropColumn("dbo.StaffUsers", "FailedLoginAttempts");
+            DropColumn("dbo.StaffUser", "IsLocked");
+            DropColumn("dbo.StaffUser", "FailedLoginAttempts");
+
             DropColumn("dbo.AdminUsers", "IsLocked");
             DropColumn("dbo.AdminUsers", "FailedLoginAttempts");
         }
