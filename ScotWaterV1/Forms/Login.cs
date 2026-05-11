8b071288494b7a67afd0c8ca14fca050d49b649f
Dean <30177459@C0548CB0021337>
2026-05-11 11:16:16 +0100

    update

--- ScotWaterV1/Forms/Login.cs
+++ ScotWaterV1/Forms/Login.cs
@@ -22,6 +22,8 @@ namespace ScotWaterV1
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
+
+
             string username = txtLoginUsername.Text.Trim();
             string password = txtLoginPassword.Text.Trim();
 
@@ -31,7 +33,6 @@ namespace ScotWaterV1
                 return;
             }
 
-            //Handles staff and admin login using the AuthService class
             var auth = new AuthService();
 
             // STAFF LOGIN
@@ -58,8 +59,8 @@ namespace ScotWaterV1
                 return;
             }
 
-            // If both failed
-            MessageBox.Show("Invalid username or password.");
+            // SHOW REAL ERROR MESSAGE (lockout, attempts left, etc.)
+            MessageBox.Show(staffError ?? adminError);
         }
 
         private void lnkForgotPassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
